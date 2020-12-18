1521813c6598f5f9b99f0f2ed475d5e00abb7e89
Shadman Shadab <oneshadab@gmail.com>
2020-12-19 02:29:53 +0600

    Add client for creating connections

---
+++ pkg/client/client.go
@@ -0,0 +1,61 @@
+package client
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+type Client struct {
+	conn net.Conn
+}
+
+func NewClient(connString string) (*Client, error) {
+	var err error
+
+	client := Client{}
+
+	client.conn, err = net.Dial("tcp", connString)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create client: %s", err)
+	}
+
+	return &client, nil
+}
+
+func (client *Client) Shell() error {
+	stdinReader := bufio.NewReader(os.Stdin)
+
+	socketReader := bufio.NewReader(client.conn)
+	socketWriter := bufio.NewWriter(client.conn)
+
+	fmt.Println("Hariken shell version v0.1")
+
+	for {
+		fmt.Printf("$ ")
+
+		line, err := stdinReader.ReadString('\n')
+		if err != nil {
+			return fmt.Errorf("Failed to read string from stdin")
+		}
+
+		_, err = socketWriter.WriteString(line)
+		if err != nil {
+			return err
+		}
+
+		err = socketWriter.Flush()
+		if err != nil {
+			return err
+		}
+
+		out, err := socketReader.ReadString('\n')
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(out)
+	}
+
+}
