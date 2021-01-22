e97824e3c0d69c83bf712bc43ee583cd0ac30966
Shadman Shadab <oneshadab@gmail.com>
2021-01-22 19:35:53 +0600

    Move commit logic to store

--- pkg/storage/memTable.go
+++ pkg/storage/memTable.go
@@ -5,44 +5,16 @@ import (
 )
 
 type MemTable struct {
-	entries   map[string]string
-	commitLog *CommitLog
+	entries map[string]string
 }
 
 // A new memtable is created from a commit log
-func NewMemTable(commitLog *CommitLog) (*MemTable, error) {
+func NewMemTable() (*MemTable, error) {
 	table := MemTable{
-		entries:   make(map[string]string),
-		commitLog: commitLog,
-	}
-
-	err := table.loadFromLog()
-	if err != nil {
-		return nil, err
+		entries: make(map[string]string),
 	}
 
 	return &table, nil
-}
-
-func (table *MemTable) loadFromLog() error {
-	for {
-		entry, err := table.commitLog.Read()
-		if err != nil {
-			return err
-		}
-
-		if entry == nil {
-			break
-		}
-
-		if entry.IsDeleted {
-			delete(table.entries, entry.Key)
-		} else {
-			table.entries[entry.Key] = entry.Val
-		}
-	}
-
-	return nil
 }
 
 func (table *MemTable) Get(key string) (*string, error) {
@@ -61,14 +33,6 @@ func (table *MemTable) Get(key string) (*string, error) {
 }
 
 func (table *MemTable) Set(key string, val string) error {
-	err := table.commitLog.Write(LogEntry{
-		Key: key,
-		Val: val,
-	})
-	if err != nil {
-		return err
-	}
-
 	table.entries[key] = val
 	return nil
 }
@@ -79,11 +43,6 @@ func (table *MemTable) Has(key string) (bool, error) {
 }
 
 func (table *MemTable) Delete(key string) error {
-	err := table.commitLog.Write(LogEntry{
-		Key:       key,
-		IsDeleted: true,
-	})
-
 	hasKey, err := table.Has(key)
 	if err != nil {
 		return err
