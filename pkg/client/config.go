ebbd9e6fe33ee34fdf9121db6bda65269abf1a94
Shadman Shadab <oneshadab@gmail.com>
2021-01-24 01:31:52 +0600

    Make config globally available

--- pkg/client/config.go
+++ pkg/client/config.go
@@ -2,8 +2,20 @@ package client
 
 import "fmt"
 
+var config *Config
+
 type Config struct {
 	ConnString string
+}
+
+func LoadConfig(cfg *Config) error {
+	err := cfg.Validate()
+	if err != nil {
+		return err
+	}
+
+	config = cfg
+	return nil
 }
 
 func (c *Config) Validate() error {
