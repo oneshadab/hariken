f770329535be9910798c57102ba55a28c793001e
Shadman Shadab <oneshadab@gmail.com>
2021-01-31 15:41:25 +0600

    Add methods for reading entries from file

--- pkg/storage/ssIndex.go
+++ pkg/storage/ssIndex.go
@@ -32,6 +32,22 @@ func newSSIndex(indexFilePath string) (*ssIndex, error) {
 	return ss, nil
 }
 
+
+func (ss *ssIndex) ReadAt(filePos int64) (*IndexFileEntry, error) {
+	_, err := ss.indexFile.Seek(filePos, os.SEEK_SET)
+	if err != nil {
+		return nil, err
+	}
+
+	entry := &IndexFileEntry{}
+	err = binary.Read(ss.indexFile, binary.LittleEndian, entry)
+	if err != nil {
+		return nil, err
+	}
+
+	return entry, nil
+}
+
 func (ss ssIndex) write(key StoreKey, dataFilePos int64) error {
 	indexEntry := IndexFileEntry{
 		key:         key,
