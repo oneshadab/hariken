8cb1a22eb10e6ecce61a7914a115662ef9d9d32e
Shadman Shadab <oneshadab@gmail.com>
2021-01-31 12:26:16 +0600

    Add separate file for Storekey

---
+++ pkg/storage/storeKey.go
@@ -0,0 +1,16 @@
+package storage
+
+const (
+	storeKeyLen = 8
+)
+
+type StoreKey [storeKeyLen]byte
+
+func (key StoreKey) isLess(otherKey StoreKey) bool {
+	for i := 0; i < storeKeyLen; i++ {
+		if key[i] < otherKey[i] {
+			return true
+		}
+	}
+	return false
+}
