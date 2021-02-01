8164b73008a0204c0501f7b4cac032403454569f
Shadman Shadab <oneshadab@gmail.com>
2021-02-01 17:13:18 +0600

    Add test for flush

--- pkg/storage/store_test.go
+++ pkg/storage/store_test.go
@@ -67,3 +67,84 @@ func TestPersistence(t *testing.T) {
 		t.Fatalf("Expected %v got %v", false, hasValue)
 	}
 }
+
+func TestFlush(t *testing.T) {
+	testData := struct {
+		key   StoreKey
+		value []byte
+	}{
+		key:   StoreKey{7},
+		value: []byte("john"),
+	}
+
+	storeDir := "temp/test"
+
+	store, err := NewStore(storeDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Part 1: Test if flushed value can be accessed
+	err = store.Set(testData.key, testData.value)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = store.Flush()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	keys, err := store.memTable.Keys()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(keys) > 0 {
+		t.Fatalf("Commit log not flushed")
+	}
+
+	value, err := store.Get(testData.key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(testData.value, value) {
+		t.Fatalf("Expected %v got %v", testData.value, value)
+	}
+
+	// Part 2: Test if flushed deleted value is properly removed
+	err = store.Delete(testData.key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = store.Flush()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hasValue, err := store.Has(testData.key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if hasValue != false {
+		t.Fatalf("Expected %v got %v", false, hasValue)
+	}
+
+	// Part 3: Test if non-flushed values are accessed properly
+	err = store.Set(testData.key, testData.value)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	value, err = store.Get(testData.key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(testData.value, value) {
+		t.Fatalf("Expected %v got %v", testData.value, value)
+	}
+}
