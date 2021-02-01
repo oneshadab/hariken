4df5896b5c0b39a227d51049dd297c56eb5e5e52
Shadman Shadab <oneshadab@gmail.com>
2021-02-01 17:13:42 +0600

    Split into separate methods

--- pkg/storage/store.go
+++ pkg/storage/store.go
@@ -12,7 +12,7 @@ type Store struct {
 	memTable  *MemTable
 	commitLog *CommitLog
 
-	SSTables [](*SSTable)
+	SSTables []*SSTable // List of sstables, youngest to oldest
 }
 
 func NewStore(dir string) (*Store, error) {
@@ -66,32 +66,6 @@ func (S *Store) Get(key StoreKey) ([]byte, error) {
 	return entry.Data, nil
 }
 
-func (S *Store) find(key StoreKey) (*LogEntry, error) {
-	// First look in memTable
-	found, err := S.memTable.hasKey(key)
-	if err != nil {
-		return nil, err
-	}
-
-	if found {
-		return S.memTable.Get(key)
-	}
-
-	// Then look for first sstable which has key
-	for _, table := range S.SSTables {
-		found, err := table.hasKey(key)
-		if err != nil {
-			return nil, err
-		}
-
-		if found {
-			return table.Get(key)
-		}
-	}
-
-	return nil, nil
-}
-
 func (S *Store) Set(key StoreKey, val []byte) error {
 	entry := &LogEntry{
 		Key:       key,
@@ -134,8 +108,33 @@ func (S *Store) Delete(key StoreKey) error {
 	return nil
 }
 
+func (S *Store) Flush() error {
+	// Create new SSTable from commit log
+	sstable, err := S.genNewSSTable()
+	if err != nil {
+		return err
+	}
+
+	// Clear the commit log
+	err = S.commitLog.Flush()
+	if err != nil {
+		return err
+	}
+
+	// Load memtable from commit log
+	err = S.syncMemtableWithLog()
+	if err != nil {
+		return err
+	}
+
+	// Add sstable to list of sstables
+	S.SSTables = append([]*SSTable{sstable}, S.SSTables...)
+
+	return nil
+}
+
 func (S *Store) syncMemtableWithLog() error {
-	err := S.commitLog.Reset()
+	err := S.memTable.Reset()
 	if err != nil {
 		return err
 	}
@@ -158,43 +157,45 @@ func (S *Store) syncMemtableWithLog() error {
 	return nil
 }
 
-func (S *Store) Flush() error {
-	// Create new SSTable from commit log
-	err := S.genNewSSTable()
+func (S *Store) find(key StoreKey) (*LogEntry, error) {
+	// First look in memTable
+	found, err := S.memTable.hasKey(key)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	// Clear the commit log
-	err = S.commitLog.Flush()
-	if err != nil {
-		return err
+	if found {
+		return S.memTable.Get(key)
 	}
 
-	// Load memtable from commit log
-	err = S.syncMemtableWithLog()
-	if err != nil {
-		return err
+	// Then look for youngest sstable which has key
+	for _, table := range S.SSTables {
+		found, err := table.hasKey(key)
+		if err != nil {
+			return nil, err
+		}
+
+		if found {
+			return table.Get(key)
+		}
 	}
 
-	return nil
+	return nil, nil
 }
 
-func (S *Store) genNewSSTable() error {
+func (S *Store) genNewSSTable() (*SSTable, error) {
 	tableId := strconv.Itoa(len(S.SSTables))
 	ssTableDir := path.Join(S.dir, "ssTables", tableId)
 
 	table, err := NewSSTable(ssTableDir)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	err = table.Build(S.memTable)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	S.SSTables = append(S.SSTables, table)
-
-	return nil
+	return table, nil
 }
