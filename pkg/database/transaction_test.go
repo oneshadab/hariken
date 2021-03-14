31d875db0621081c369580b2cd645f5a8595d301
Shadab <oneshadab@gmail.com>
2021-03-14 18:35:59 +0600

    Make test more deterministic

--- pkg/database/transaction_test.go
+++ pkg/database/transaction_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
@@ -11,6 +12,7 @@ func TestIsolation(t *testing.T) {
 		{"name": "alex"},
 	}
 
+	// Setup reader and writer thread
 	writer := startWorker()
 	reader := startWorker()
 
@@ -25,43 +27,50 @@ func TestIsolation(t *testing.T) {
 	writeTx.UseTable("test")
 	readTx.UseTable("test")
 
+	// Part 1: Write data in writer thread and read data from reader thread
 	writer.exec(func() {
 		writeTx.InsertRow(testData[0])
 	})
+
 	writer.exec(func() {
 		writeTx.Commit()
 	})
 
-	time.Sleep(50 * time.Millisecond)
+	writer.join()
 
 	rowId := writeTx.Result[0].Id()
 	reader.exec(func() {
 		readTx.FetchRow(rowId)
 	})
 
-	time.Sleep(50 * time.Millisecond)
+	reader.join()
 
 	if testData[0]["name"] != readTx.Result[0].Column["name"] {
 		t.Fatalf("Expected %s got %s\n", testData[0]["name"], readTx.Result[0].Column["name"])
 	}
 
-	// Part 2
+	// Part 2: Write data in writer thread but try to read it before it's commited
 
 	writer.exec(func() {
 		writeTx.UpdateAll(testData[1])
 	})
-	time.Sleep(50 * time.Millisecond)
+	writer.join()
 
 	reader.exec(func() {
 		readTx.FetchRow(rowId)
 	})
+
+	// Give the reader some time to try to read the data
 	time.Sleep(50 * time.Millisecond)
 
 	writer.exec(func() {
 		writeTx.Commit()
 	})
-	time.Sleep(50 * time.Millisecond)
 
+	writer.join()
+	reader.join()
+
+	// Reader should see the updated data, even though read was issued before the updated data was commited
 	if testData[1]["name"] != readTx.Result[0].Column["name"] {
 		t.Fatalf("Expected %s got %s\n", testData[1]["name"], readTx.Result[0].Column["name"])
 	}
@@ -69,6 +78,7 @@ func TestIsolation(t *testing.T) {
 
 type Worker struct {
 	commands chan func()
+	wg       sync.WaitGroup
 }
 
 // Creates a worker and starts it in the background
@@ -80,6 +90,7 @@ func startWorker() *Worker {
 	go func() {
 		for cmd := range worker.commands {
 			cmd()
+			worker.wg.Done()
 		}
 	}()
 
@@ -87,5 +98,10 @@ func startWorker() *Worker {
 }
 
 func (w *Worker) exec(cmd func()) {
+	w.wg.Add(1)
 	w.commands <- cmd
+}
+
+func (w *Worker) join() {
+	w.wg.Wait()
 }
